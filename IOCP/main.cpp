cf83b68e82c6d7522c9017517630de1980f3170a
admin11 <admin11@XHQ>
2018-10-31 08:25:11 +0800

    update

--- IOCP/main.cpp
+++ IOCP/main.cpp
@@ -2,9 +2,16 @@
 //
 
 #include "stdafx.h"
+#include <list>
 #include "IOCP.h"
 #include "XhqSocket.h"
-
+#define  OP_ACCEPT 1
+typedef struct tagMyOverLapped
+{
+    WSAOVERLAPPED m_WsaOverLapped;
+    int           m_nOperation;
+    SOCKET        m_ClientSocket;
+}MYOVERLAPPED;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -12,9 +19,14 @@ int _tmain(int argc, _TCHAR* argv[])
   CIOCP Iocp;
   Iocp.CreateIoCompletionPortObj();
   SOCKET server;
+
   SOCKADDR_IN skAddr;
   DWORD dwRetBytes = 0;
+  char chBuff[1024] = { 0 };
+  DWORD dwRecvBytes = 0;
   server = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+  std::list<MYOVERLAPPED*> lstClient;
   skAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
   skAddr.sin_port = htons(5166);
   int nRet = ::bind(server, (sockaddr*)&skAddr, sizeof(skAddr));
@@ -40,13 +52,30 @@ int _tmain(int argc, _TCHAR* argv[])
   }
 
   nRet = ::listen(server, 200);
-
   bool bIsReciveConnect = true;
+  MYOVERLAPPED* pNewClient = NULL; new MYOVERLAPPED;
+
   while (true)
   {
     if (bIsReciveConnect)
     {
-      lpAcceptEx(server,)
+        SOCKET client;
+        client = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        pNewClient = new MYOVERLAPPED;
+        pNewClient->m_ClientSocket = client;
+        pNewClient->m_nOperation = OP_ACCEPT;
+        bRet = lpAcceptEx(server,
+                          client,
+                          chBuff,
+                          1024 - (sizeof(SOCKADDR_IN) + 16) * 2,
+                          sizeof(SOCKADDR_IN) + 16,
+                          sizeof(SOCKADDR_IN) + 16,
+                          &dwRecvBytes,
+                          &pNewClient->m_WsaOverLapped);
+        if (!bRet)
+        {
+            printf("AccpetEx Failed Error Code:[%d]", GetLastError());
+        }
     }
   }
 
